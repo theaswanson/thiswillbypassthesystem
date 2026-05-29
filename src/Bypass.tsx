b7de4ba1c995f0abce4ace7bb64170acb89560d9
Adam Swanson <adam.swanson.98@gmail.com>
2026-05-29 17:27:51 -0400

    Add button to copy to clipboard

--- src/Bypass.tsx
+++ src/Bypass.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import Markdown from "react-markdown";
 
 export type BypassModel = {
@@ -11,17 +12,39 @@ export const Bypass = ({
 }: {
   bypass: BypassModel;
   isLoading: boolean;
-}) => (
-  <div>
-    <hr />
-    {isLoading ? (
-      <code>Loading...</code>
-    ) : (
-      <div>
-        <h2>Bypass #{id}</h2>
-        <Markdown>{contents}</Markdown>
-      </div>
-    )}
-    <hr />
-  </div>
-);
+}) => {
+  const [copySuccessful, setCopySuccessful] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    setCopySuccessful(null);
+  }, [id]);
+
+  const copyToClipboard = async () => {
+    try {
+      await navigator.clipboard.writeText(contents);
+      setCopySuccessful(true);
+    } catch {
+      setCopySuccessful(false);
+    }
+  };
+
+  return (
+    <div>
+      <hr />
+      {isLoading ? (
+        <code>Loading...</code>
+      ) : (
+        <div>
+          <h2>Bypass #{id}</h2>
+          <button onClick={copyToClipboard}>
+            {copySuccessful === null && <span>Copy 📋</span>}
+            {copySuccessful === true && <span>Copied ✅</span>}
+            {copySuccessful === false && <span>Failed to copy ❌</span>}
+          </button>
+          <Markdown>{contents}</Markdown>
+        </div>
+      )}
+      <hr />
+    </div>
+  );
+};
